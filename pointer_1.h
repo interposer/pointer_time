9ccc3e28769cf8a2578d7d9fd0e9539c39b0478e
interposer <jayme.martin@gmail.com>
2014-12-17 22:47:02 -0800

    new file

---
+++ pointer_1.h
@@ -0,0 +1,31 @@
+//
+//  pointer_1.h
+//  
+//
+//  Created by interposer on 12/17/14.
+//
+//
+
+#ifndef _pointer_1_h
+#define _pointer_1_h
+
+//Allocates memory for username. Returns size actually allocated.
+int allocate_username(char** username, int size);
+
+int client(){
+    
+    char* my_username_buffer;
+    
+    
+    //client calls a function that allocates memory for a username
+    //and returns a pointer to said memory stored in arg passed in.
+    //in this case the client function will stores this pointer
+    // in my_username_buff
+    
+    int alloc_size = allocate_username(&my_username_buffer, 256);
+    
+    printf("username[%d]: %s", alloc_size, my_username_buff);
+    
+}
+
+#endif
